6854c86572ccb9e21b100441a16a60efe43a82a6
Benjamin Price <github@benprice.ca>
2021-12-02 06:34:20 +0900

    Basic solution no testing

--- dec01-1/src/main.rs
+++ dec01-1/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut previous = 0;
+    let mut count = 0;
+
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap().parse().unwrap();
+        if index != 0 && line > previous {
+            count = count + 1;
+        }
+        previous = line;
+    }
+
+    println!("{}", count);
 }
